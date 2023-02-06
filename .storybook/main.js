92e1617a2cbaff59ee2108bc8f1f80773105bf39
wanglan <wlan.orange@qq.com>
2023-02-06 11:38:23 +0800

    🐞 fix: test

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,22 @@
+
+const path = require("path")
+const fs = require("fs")
+
+// [Workaround] This logic means `"../src/components/*.stories.@(js|jsx|ts|tsx)"` but it's much faster.
+function getStories(pkg) {
+  const scope = pkg ? [pkg] : fs.readdirSync("src/components")
+  return scope
+    .map((package) => `src/components/${package}`)
+    .filter((storyDir) => fs.existsSync(storyDir))
+    .map((storyDir) => `../${storyDir}/*.stories.@(js|jsx|ts|tsx)`)
+}
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  core: {
+    builder: "@storybook/builder-webpack5",
+    disableTelemetry: true,
+  },
+  stories: getStories(),
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
